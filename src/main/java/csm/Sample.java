1311221e0f91845638f38db660492cf28d23c445
programming-maestro <csm.exclusive@gmail.com>
2024-08-28 18:50:59 +0530

    Loop added to main method.

--- src/main/java/csm/Sample.java
+++ src/main/java/csm/Sample.java
@@ -6,11 +6,15 @@ public class Sample {
 
 	public static void main(String[] args) {
 		int i = 0;
+
 		System.out.println("Main method invoked: ");
-		try {
-			System.out.println(++i);
-			Thread.sleep(10000);
-		} catch (Exception e) {
+
+		while (true & i < 50) {
+			try {
+				System.out.println(++i);
+				Thread.sleep(10000);
+			} catch (Exception e) {
+			}
 		}
 
 	}
