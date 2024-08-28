c03e619e42acfdc25305d1435b85d2287f480910
programming-maestro <csm.exclusive@gmail.com>
2024-08-28 18:27:00 +0530

    pom.xml correction and main method cyclic

--- src/main/java/csm/Sample.java
+++ src/main/java/csm/Sample.java
@@ -5,9 +5,11 @@ import java.util.Scanner;
 public class Sample {
 
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Write anything: ");
-		String s = input.next();
-		System.out.println("This was the command line argument: " + s);
+		while (true) {
+			Scanner input = new Scanner(System.in);
+			System.out.print("Write anything: ");
+			String s = input.next();
+			System.out.println("This was the command line argument: " + s);
+		}
 	}
 }
